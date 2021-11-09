80670dce4c1be8cfa72dac981ff74c7ecce524fe
WesleyBryie <93141203+WesleyBryie@users.noreply.github.com>
2021-11-09 11:42:35 -0500

    fg

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// define creator and proper domain
+// define creator info
 var creator = "Wesley Bryie";
 var domain = "wesbryie.cf"
-console.log("Made by: ",creator," in 2021. This site was designed for ",domain,". If you happen to find this site on anyother domain not owned by me, please contact me at wesleybryie@protonmail.com.");
+var email = "wesleybryie@protonmail.com"
+console.log("Made by: ",creator," in 2021. This site was designed for ",domain,". If you happen to find this site on anyother domain not owned by me, please contact me at ",email);
 
