a01a0d42ef47fb0e9eb27ad78f03c08f2d640517
WesleyBryie <93141203+WesleyBryie@users.noreply.github.com>
2021-11-09 11:43:34 -0500

    jfundfg

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
 var creator = "Wesley Bryie";
 var domain = "wesbryie.cf"
 var email = "wesleybryie@protonmail.com"
+
+// send notice in javascript log / console
 console.log("Made by: ",creator," in 2021. This site was designed for ",domain,". If you happen to find this site on anyother domain not owned by me, please contact me at ",email);
 
