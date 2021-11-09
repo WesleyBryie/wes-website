79636fee23eb27bfc51e4aec943e35aa8a7e65c7
WesleyBryie <93141203+WesleyBryie@users.noreply.github.com>
2021-11-09 11:35:59 -0500

    ijgfij

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log("Made by: Wesley Bryie in 2021. This site was designed for wesbryie.cf. If you happen to find this site on anyother domain not owned by me, please contact me at wesleybryie@protonmail.com.")
