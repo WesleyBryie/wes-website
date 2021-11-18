31200fcd8b921ee7b7dc7a57852f025c5ac74948
Wes Bryie <93141203+WesleyBryie@users.noreply.github.com>
2021-11-18 10:32:35 -0500

    javascript go brr

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,12 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
-console.log("Copyright *my code only, anything that is not my code is published and copyrighted by their respective owners* Wesley Bryie, 2021.")
+var domain = wesbryie.cf;
+
+console.log("Copyright *my code only, anything that is not my code is published and copyrighted by their respective owners* Wesley Bryie, 2021.");
+console.log("This website was designed and published to https://wesbryie.cf/. If you see it on a domain that is not wesbryie.cf please contact me at wesleybryie@protonmail.com.");
+
+if (domain = "wesbryie.cf")
+console.log("This websites javascript code says that it is correct.");
+    
+else console.log("I SEE YOU.");
